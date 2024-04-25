72b9931fe3c6e460d15210c85251bb29f778719c
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-25 16:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let colorSelect = document.getElementById("colorSelection"); // Assuming ID is "colorSelection"
-let removeButton = document.querySelector("input[type='button']");
+let a = document.getElementById("colorSelection");
+let b = document.querySelector("input[type='button']");
 
-removeButton.addEventListener("click", function() {
-  const selectedIndex = colorSelect.selectedIndex;
-
-  // Check if a valid option is selected (not -1 for no selection)
-  if (selectedIndex !== -1) {
-    colorSelect.remove(selectedIndex);
-  } else {
-    alert("Please select an option to remove!");
-  }
+b.addEventListener("click", function() {
+    const s = a.selectedIndex;
+    if (s !== -1) {
+        a.options[s].remove();
+    } else {
+        alert('Please select an option to remove');
+    }
 });
+
