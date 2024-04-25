ab3a5115841dc1bcd5f3ac116dc523c3f9340fb9
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-25 16:19:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let a = document.getElementById("colorSelection");
+let b = document.querySelector("input[type='button']");
+
+b.addEventListener("click",function (params) {
+	const s = a.selectedIndex();
+	if(s !== -1){
+		a.remove(s);
+	}
+	else{
+		alert('please select an option to remove');
+}
+});
