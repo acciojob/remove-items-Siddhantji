6b5fa5c4aaaf9653232604c32815c1f3d348cb50
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-25 16:35:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let b = document.querySelector("input[type='button']");
 b.addEventListener("click",function (params) {
 	const s = a.selectedIndex;
 	if(s !== -1){
-		a.remove(s);
+		a.options[s].remove();
 	}
 	else{
 		alert('please select an option to remove');
